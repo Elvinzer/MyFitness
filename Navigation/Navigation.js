8cf13cbafa557dc83e565d3572d51d038d6ca5a4
Elvinzer <55585723+Elvinzer@users.noreply.github.com>
2020-06-14 19:19:03 +0200

    qzd

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,10 +1,23 @@
 // Navigation/Navigation.js
 
 import { createStackNavigator} from 'react-navigation-stack'
-import { createAppContainer } from "react-navigation";
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator,DrawerItems} from 'react-navigation-drawer';
 
 import Search from '../Components/Search'
 import TrainingDetail from '../Components/TrainingDetail'
+
+//On initialise le menu latéral "Hamburger"
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -13,9 +26,11 @@ const SearchStackNavigator = createStackNavigator({
       title: 'Selection du training'
     }
   },
-  TrainingDetail: { // Encore une fois j'ai mis le même nom que celui du component mais libre à vous de choisir un nom différent
-    screen: TrainingDetail
-  }
-})
+  TrainingDetail: { // j'ai mis le même nom que celui du component mais libre de choisir un nom différent
+    screen: TrainingDetail,
+    navigationOptions: {
+      title: 'Detail du training'
+    }
+}})
 
 export default createAppContainer(SearchStackNavigator)

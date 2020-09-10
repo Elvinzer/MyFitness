f6ae9e640454b35b5021d520db68226195493a25
Elvinzer <55585723+Elvinzer@users.noreply.github.com>
2020-09-10 21:51:52 +0200

    changement release date

--- Helpers/trainingList.js
+++ Helpers/trainingList.js
@@ -6,7 +6,7 @@ export default dataTrainings = [
       vote_average:7.2,
       title:"Push",
       original_title:"Programme Push",
-      release_date:"12/06/2019",
+      release_date:"30/07/2019",
       poster_path:"https://static.vecteezy.com/system/resources/previews/000/162/096/original/man-doing-push-up-vector-illustration.jpg",
       overview:"Il s'agit d'un programme incluant des mouvements de poussés (ex: Développé couché, barre au front etc.)",
    },
@@ -15,7 +15,7 @@ export default dataTrainings = [
       vote_average:8.1,
       title:"Pull",
       original_title:"Programme Pull",
-      release_date:"12/06/2020",
+      release_date:"31/07/2020",
       poster_path:"https://www.pullupfitness.com/wp-content/uploads/2017/06/tractions-pronation-300x300.png",
       overview:"Il s'agit d'un programme incluant des mouvements de tirage (ex: Tractions, Curl à la barre Z etc.)",
    }
